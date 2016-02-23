02d5f022f251789e02ddbdb7b09844c941d99753
John McCallum <cfljam@users.noreply.github.com>
2016-02-24 11:43:16 +1300

    prototype design module working

--- pcr_marker_design/design.py
+++ pcr_marker_design/design.py
@@ -25,10 +25,29 @@ class PrimerDesign:
     """A primer design object that is primed
     with genome reference and variant data
     """
-    def __init__(self, reference):
+    def __init__(self, reference,annot_file,desc):
+        """
+        Usage:  PrimerDesign(reference, annotation, description)
+        Initialise a design object witha  reference assembly and
+        annotation file(s)
+        """
         self.reference = Fasta(reference)
+        self.annotations=BedTool(annot_file)
+        self.desc=desc
+        self.genome=self.reference.filename.replace("fasta","fasta.fai")
 
 
-    def getseqslice(self,contig,start,end):
-        myslice=self.reference[contig][start:end]
-        return myslice.seq
+    def getseqslicedict(self,target,max_size):
+        """Pass a target to a designer and get a dictionary
+        slice that we can pass to P3
+        """
+        target_int=target.slop(b=max_size,g=self.genome)
+        offset=target_int[0].start
+        sldic=dict(SEQUENCE_ID=self.desc)
+        sldic['TARGET_ID']=str(target[0].chrom+ "_" + str(target_int[0].start) +"_" + str(target_int[0].end))
+        sldic['SEQUENCE_TEMPLATE']=str(self.reference[target[0].chrom][target_int[0].start:target_int[0].end].seq)
+        slice_annot=[(X.start -offset,X.length) for X in (self.annotations - target) if (X.chrom==target[0].chrom) & \
+                     (X.start > target_int[0].start) & (X.end < target_int[0].end)]
+        sldic['SEQUENCE_EXCLUDED_REGION']=slice_annot
+        sldic['SEQUENCE_TARGET']= (target[0].start -offset,target[0].length)
+        return sldic
--- test/test_design.py
+++ test/test_design.py
@@ -6,7 +6,8 @@ from pyfaidx import Fasta
 class TestDesign:
     def test_getseqslice(self):
         test_seq="./test/test-data/targets.fasta"
+        annfile= "./test/test-data/targets.gff"
         k69_93535_250_290='GACAAAGAGAAAATCCTCAAATCCGGCCTCGTCAACCACA'
-        designer = d.PrimerDesign(test_seq)
+        designer = d.PrimerDesign(test_seq,annfile)
         seqslice=designer.getseqslice('k69_93535',250,290)
         assert seqslice == k69_93535_250_290
