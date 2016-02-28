8cdfb2d30e308481a2f7fd9ca489f6f406d7563c
john mccallum <cfljam@users.noreply.github.com>
2016-02-28 16:03:10 +1300

    added vcf for tests. Not passing yet

--- pcr_marker_design/design.py
+++ pcr_marker_design/design.py
@@ -21,6 +21,7 @@ return an iterable  of primer sets
 
 from pyfaidx import Fasta
 from pybedtools import BedTool
+import vcf
 
 class PrimerDesign:
     """A primer design object that is primed
@@ -47,7 +48,7 @@ class PrimerDesign:
         sldic=dict(SEQUENCE_ID=self.desc)
         sldic['TARGET_ID']=str(target[0].chrom+ "_" + str(target_int[0].start) +"_" + str(target_int[0].end))
         sldic['SEQUENCE_TEMPLATE']=str(self.reference[target[0].chrom][target_int[0].start:target_int[0].end].seq)
-        slice_annot=[(X.start -offset,X.length) for X in (self.annotations - target) if (X.chrom==target[0].chrom) & \
+        slice_annot=[(X.start-offset,X.length) for X in (self.annotations - target) if (X.chrom==target[0].chrom) & \
                      (X.start > target_int[0].start) & (X.end < target_int[0].end)]
         sldic['SEQUENCE_EXCLUDED_REGION']=slice_annot
         sldic['SEQUENCE_TARGET']= (target[0].start -offset,target[0].length)
@@ -77,15 +78,12 @@ class VcfPrimerDesign:
         target_chrom=target[0].chrom
         target_start=target_int[0].start
         target_end=target_int[0].end
-        offset=target_int[0].start
         sldic=dict(SEQUENCE_ID=self.desc)
         sldic['TARGET_ID']=target_chrom + "_" + str(target_start) +"_" + str(target_end)
         sldic['SEQUENCE_TEMPLATE']=str(self.reference[target_chrom][target_start:target_end].seq)
-        #slice_annot=[(X.start -offset,X.length) for X in (self.annotations - target) if (X.chrom==target[0].chrom) & \
-         #           (X.start > target_int[0].start) & (X.end < target_int[0].end)]
         slice_vars=[target_chrom + " " + str(X.start)+ " " +str(X.end) for X in self.vcf.fetch(target_chrom,target_start,target_end)]
         slice_annot=BedTool("\n".join(slice_vars),from_string=True)
         slice_annot=slice_annot-target
-        sldic['SEQUENCE_EXCLUDED_REGION']=[(X.start,X.length) for X in slice_annot]
-        sldic['SEQUENCE_TARGET']= (target[0].start -offset,target[0].length)
+        sldic['SEQUENCE_EXCLUDED_REGION']=[(X.start - target_start,X.length) for X in slice_annot]
+        sldic['SEQUENCE_TARGET']= (target[0].start - target_start,target[0].length)
         return sldic
