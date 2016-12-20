2e78d4b8a99acb7ac8cb0ac1fe8c84c59901ed85
Tim Millar <tim.millar@plantandfood.co.nz>
2016-12-21 12:24:59 +1300

    pep8 whitespace for run_p3.py

--- pcr_marker_design/run_p3.py
+++ pcr_marker_design/run_p3.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
-##run primer3 by passing Python dictionary
 
-#run_P3.py
+import primer3
 
-p3_globals={
+# run primer3 by passing Python dictionary
+
+# run_P3.py
+
+p3_globals = {
         'PRIMER_OPT_SIZE': 20,
         'PRIMER_PICK_INTERNAL_OLIGO': 1,
         'PRIMER_INTERNAL_MAX_SELF_END': 8,
@@ -23,17 +26,17 @@ p3_globals={
         'PRIMER_MAX_SELF_END': 8,
         'PRIMER_PAIR_MAX_COMPL_ANY': 12,
         'PRIMER_PAIR_MAX_COMPL_END': 8,
-        'PRIMER_PRODUCT_SIZE_RANGE': [[60,200]],
+        'PRIMER_PRODUCT_SIZE_RANGE': [[60, 200]],
     }
 
-import primer3
 
-##call P3 with dict of args, returns dict, no exception handling
-def run_P3(target_dict,global_dict):
-    P3_dict=primer3.bindings.designPrimers(target_dict,global_dict)
-    ##return iterable list
-    primer_list=[dict(PRIMER_RIGHT_SEQUENCE=P3_dict.get('PRIMER_RIGHT_'+ str(X) + '_SEQUENCE'),\
-    PRIMER_LEFT=P3_dict.get('PRIMER_LEFT_'+ str(X) ),\
-    PRIMER_RIGHT=P3_dict.get('PRIMER_RIGHT_'+ str(X) ),\
-    PRIMER_LEFT_SEQUENCE=P3_dict.get('PRIMER_LEFT_'+ str(X) + '_SEQUENCE')) for X in range(0,int(P3_dict.get('PRIMER_RIGHT_NUM_RETURNED'))-1)]
-    return(primer_list)
+# call P3 with dict of args, returns dict, no exception handling
+def run_P3(target_dict, global_dict):
+    P3_dict = primer3.bindings.designPrimers(target_dict, global_dict)
+    # return iterable list
+    primer_list = [dict(PRIMER_RIGHT_SEQUENCE=P3_dict.get('PRIMER_RIGHT_' + str(X) + '_SEQUENCE'),
+                        PRIMER_LEFT=P3_dict.get('PRIMER_LEFT_' + str(X)),
+                        PRIMER_RIGHT=P3_dict.get('PRIMER_RIGHT_' + str(X)),
+                        PRIMER_LEFT_SEQUENCE=P3_dict.get('PRIMER_LEFT_' + str(X) + '_SEQUENCE')) for
+                   X in range(0, int(P3_dict.get('PRIMER_RIGHT_NUM_RETURNED')) - 1)]
+    return primer_list
